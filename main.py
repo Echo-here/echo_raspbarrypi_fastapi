3580cece72e6b3bc85c2775fe1625618d9caae4e
bitbyte08 <me@bitworkspace.kr>
2025-07-17 01:16:19 +0900

    fix :: read_serial GC 강제 삭제 문제 해결

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from model import Order
 from config import SERIAL_PORT, BAUDRATE
 app = FastAPI()
 now_serial_data = None
+serial_task = None  # Task 핸들 저장
 
 async def read_serial():
     global now_serial_data
@@ -27,8 +28,18 @@ async def read_serial():
 
 @app.on_event("startup")
 async def start_serial_reader():
-    asyncio.create_task(read_serial())
+    global serial_task
+    serial_task = asyncio.create_task(read_serial())
 
+@app.on_event("shutdown")
+async def stop_serial_reader():
+    global serial_task
+    if serial_task:
+        serial_task.cancel()
+        try:
+            await serial_task
+        except asyncio.CancelledError:
+            print("[Shutdown] Serial reader cancelled cleanly.")
 @app.get("/stock")
 async def get_serial_data(type: Optional[str] = None):
     try:
