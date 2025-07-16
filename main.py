f43c8a2817c4a3ae130038c9b95c7332c4ed669a
bitbyte08 <me@bitworkspace.kr>
2025-07-16 23:53:33 +0900

    feat :: 시리얼 수신

--- main.py
+++ main.py
@@ -1,13 +1,38 @@
+# serial_app.py
+import asyncio
+from typing import Optional
+
+import serial_asyncio
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
+now_serial_data = None
 
+async def read_serial():
+    global now_serial_data
+    reader, _ = await serial_asyncio.open_serial_connection(
+        url="/dev/pts/7", baudrate=9600
+    )
+    while True:
+        try:
+            line = await reader.readline()
+            decoded = json.loads(line)
+            print(f"[Serial] {decoded["sugar"]}")
+            now_serial_data = decoded
+        except Exception as e:
+            print(f"[Error] {e}")
+            await asyncio.sleep(1)
 
-@app.get("/")
-async def root():
-  return {"message": "Hello World"}
+@app.on_event("startup")
+async def start_serial_reader():
+    asyncio.create_task(read_serial())
 
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-  return {"message": f"Hello {name}"}
+@app.get("/stock")
+async def get_serial_data(type: Optional[str] = None):
+    try:
+        if type:
+            return {type: now_serial_data[type]}
+        return now_serial_data
+    except Exception as e:
+        return {"error": e}
